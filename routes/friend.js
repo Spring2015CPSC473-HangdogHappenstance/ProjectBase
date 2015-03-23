bf7d8f48b884be3e59c9564e93b28358c1af9ef5
Zaephor <github@draconrose.com>
2015-03-22 17:18:49 -0700

    Shifted static data sources around

--- routes/friend.js
+++ routes/friend.js
@@ -4,16 +4,16 @@
 
 /* List user's current friends */
 exports.list = function(req, res){
-    console.log(req);
-    res.render('friend/list', { title: 'List of friends', friends: somefriends });
+    var someFriends = [{"id":1,"name":"UserA","match":{"Movies":90,"TV":70,"Books":10}},{"id":2,"name":"UserB","match":{"Movies":40,"TV":80,"Books":55}},{"id":3,"name":"UserC","match":{"Movies":10,"TV":10,"Books":10}}];
+    res.render('friend/list', { title: 'List of friends', friends: someFriends });
 };
 /* Display the find-people screen */
 exports.find = function(req, res){
-    console.log(req);
-    res.render('friend/find', { title: 'Friend search prompt and results' });
+    var somePeople = [{"id":1,"name":"UserA","match":{"Movies":100,"TV":95,"Books":80}},{"id":2,"name":"UserB","match":{"Movies":90,"TV":80,"Books":10}},{"id":3,"name":"UserC","match":{"Movies":80,"TV":10,"Books":1}}];
+    res.render('friend/find', { title: 'Friend search prompt and results', results: somePeople });
 };
 /* Display recommended friends */
 exports.discover = function(req, res){
-    console.log(req);
-    res.render('friend/discover', { title: 'Recommended friends/Discover friends' });
+    var somePeople = [{"id":1,"name":"UserA","match":{"Movies":100,"TV":95,"Books":80}},{"id":2,"name":"UserB","match":{"Movies":90,"TV":80,"Books":10}},{"id":3,"name":"UserC","match":{"Movies":80,"TV":10,"Books":1}}];
+    res.render('friend/discover', { title: 'Recommended friends/Discover friends', results: somePeople });
 };
